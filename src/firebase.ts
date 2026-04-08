bcb9c434e7512d6eee1f1b335485c3d2f8b276e6
shaiknisar2503 <shaiknisar2503@gmail.com>
2026-04-08 19:07:26 +0530

    Update Firestore initialization to use default config

--- src/firebase.ts
+++ src/firebase.ts
@@ -7,5 +7,5 @@ import firebaseConfig from '../firebase-applet-config.json';
 
 // Initialize Firebase SDK
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app, firebaseConfig.firestoreDatabaseId);
+export const db = getFirestore(app);
 export const auth = getAuth(app);
